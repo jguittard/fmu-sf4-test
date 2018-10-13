8de83aee29512f4605e89b0d5126ca84a0ea7fba
Julien Guittard <julien.guittard@me.com>
2018-10-13 16:42:07 -0400

    IDE project setup, XDebug, Category entity

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\Category;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -19,10 +20,10 @@ class LandingController extends AbstractController
     public function categories()
     {
         $categories = [
-            'Frais',
-            'Produits Laitiers',
-            'Beurres',
-            'Beurre sans sel',
+            new Category('Frais'),
+            new Category('Produits Laitiers'),
+            new Category('Beurres'),
+            new Category('Beurre sans sel'),
         ];
 
         return $this->render('landing/categories.html.twig', compact('categories'));
